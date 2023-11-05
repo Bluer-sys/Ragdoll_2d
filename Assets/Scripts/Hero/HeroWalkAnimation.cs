b48337e0c7178c02d2052ff2cbe2beaf7ca158a8
Ilya Novikov <ilianovi@mail.ru>
2023-11-05 17:06:01 +0300

    Walk Animation

--- Assets/Scripts/Hero/HeroWalkAnimation.cs
+++ Assets/Scripts/Hero/HeroWalkAnimation.cs
@@ -1,15 +1,25 @@
 namespace Game.Hero
 {
+	using Game.Input;
 	using UnityEngine;
 
 	[RequireComponent(typeof(Animator))]
 	public sealed class HeroWalkAnimation : MonoBehaviour
 	{
+		static readonly int IsMoving = Animator.StringToHash( "IsWalking" );
+
+		[SerializeField] PlayerInput _input;
+			
 		Animator _animator;
-		
+
 		void Start()
 		{
 			_animator = GetComponent<Animator>();
+		}
+
+		void Update()
+		{
+			_animator.SetBool( IsMoving, _input.MoveAxis != 0 );
 		}
 	}
 }
